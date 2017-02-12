a213f670c68a04d1d6b98ce0b222691133cffd87
mike <mike.kim07@gmail.com>
2017-02-11 22:50:41 -0600

    Fixed color and beat scaling

--- app/src/main/java/com/magkim/metronome/MainActivity.java
+++ app/src/main/java/com/magkim/metronome/MainActivity.java
@@ -1,77 +1,78 @@
 package com.magkim.metronome;
 
-import android.gesture.Gesture;
 import android.graphics.Color;
-import android.net.Uri;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
-    private int MAX_BEATS = 144;
-    private int MIN_BEATS = 0;
+public class MainActivity extends AppCompatActivity
+        implements GestureDetector.OnGestureListener {
 
-    private int BLUE = 144;
-    private int RED = 144;
+    private Timer mTimer;
+    private ToneGenerator mToneGenerator = new ToneGenerator(AudioManager.STREAM_ALARM, 50);
+    private Boolean mTimerStarted = false;
 
-    private int MAX_SCROLL  = 20;
-    private int MIN_SCROLL  = -20;
+    private float BEATS     = 60;         // Default
+    private float MAX_BEATS = 208;
+    private float MIN_BEATS = 40;
+    private float BEAT_COLOR_SCALE  = 255 / MAX_BEATS;
+    private float MAX_SCROLL = 20;
 
-    private RelativeLayout getLayout () {
-        return (RelativeLayout) findViewById (R.id.activity_main);
-    }
+    private void setBackground ()
+    {
+        final int coefficient = Math.round(BEATS * BEAT_COLOR_SCALE);
 
-    private float clipScroll (float val) {
-        if (val > MAX_SCROLL)
-            return MAX_SCROLL;
-        if (val < MIN_SCROLL)
-            return MIN_SCROLL;
-        return val;
-    }
+        final int blue = 255 - coefficient;
+        final int red = coefficient;
 
-    private int clipColor (float val) {
-        if (val > 255)
-            return 255;
-        if (val < 0)
-            return 0;
-        return (int)val;
+        ((TextView) findViewById(R.id.bpm)).setText(String.valueOf((int)BEATS));
+        mLayout.setBackgroundColor(Color.argb(255, red, 0, blue));
     }
 
     private GestureDetectorCompat mDetector;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+    private RelativeLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState_) {
         super.onCreate(savedInstanceState_);
+
         setContentView(R.layout.activity_main);
 
         mDetector = new GestureDetectorCompat(this, this);
+        mLayout = (RelativeLayout) findViewById (R.id.activity_main);
 
-        getLayout ().setBackgroundColor(Color.argb(255, RED, 0, BLUE));
+        setBackground ();
     }
 
     @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-                            float distanceY) {
+    public boolean onScroll(
+            MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
 
-        final float scaledValue = clipScroll (distanceY);
+        if (distanceY > MAX_SCROLL)
+            distanceY = MAX_SCROLL;
+        else if (distanceY < -MAX_SCROLL)
+            distanceY = -MAX_SCROLL;
 
-        getLayout ().setBackgroundColor(Color.argb(255, RED + clipColor(RED + scaledValue), 0,
-                BLUE + clipColor(BLUE + scaledValue)));
+        BEATS += distanceY;
 
+        if (BEATS < MIN_BEATS)
+            BEATS = MIN_BEATS;
+        else if (BEATS > MAX_BEATS)
+            BEATS = MAX_BEATS;
+
+        setBackground();
         return true;
     }
 
@@ -104,5 +105,26 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     @Override
     public boolean onSingleTapUp(MotionEvent event) {
         return true;
+    }
+
+    public void handleButtonClicked(View view) {
+        Button button = (Button)findViewById(R.id.button);
+
+        if (mTimerStarted) {
+            mTimer.cancel();
+            button.setText("Start");
+        }
+        else {
+            mTimer = new Timer();
+            mTimer.scheduleAtFixedRate(new TimerTask() {
+                                           @Override
+                                           public void run() {
+                                               mToneGenerator.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 100);
+                                           }
+                                       }, 1000, (int)(BEATS) * 100);
+            button.setText("Stop");
+        }
+
+        mTimerStarted = ! mTimerStarted;
     }
 }
